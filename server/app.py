da089f8e97f79226ae9a2846c02ed22b00028bf7
odiedo <odiedopaul@gmail.com>
2024-07-10 10:28:43 +0300

    code challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,54 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict() for restaurant in restaurants], 200
+
+class RestaurantDetail(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            return restaurant.to_dict(rules=('restaurant_pizzas', 'restaurant_pizzas.pizza')), 200
+        return {'error': 'Restaurant not found'}, 404
+
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            # First delete associated RestaurantPizzas
+            RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+            # Then delete the restaurant
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return {'error': 'Restaurant not found'}, 404
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict() for pizza in pizzas], 200
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            return new_restaurant_pizza.to_dict(rules=('pizza', 'restaurant')), 201
+        except Exception as e:
+            db.session.rollback()
+            return {'errors': [str(e)]}, 422
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantDetail, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
